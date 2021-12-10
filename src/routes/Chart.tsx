3ba41e4850d724690780ca8eaeea1c89cbd53fbc
sdkiq1234 <sdkiq123@naver.com>
2021-12-10 11:00:00 +0900

    conclusion

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -17,8 +17,12 @@ interface ChartProps {
   coinId: string;
 }
 function Chart({ coinId }: ChartProps) {
-  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
-    fetchCoinHistory(coinId)
+  const { isLoading, data } = useQuery<IHistorical[]>(
+    ["ohlcv", coinId],
+    () => fetchCoinHistory(coinId),
+    {
+      refetchInterval: 10000,
+    }
   );
   return (
     <div>
